f6fbc17a86a953855569edd8adcff1851f6b19f2
Maksym Ivanchenko <github@ivanchenko.io>
2023-03-21 20:14:18 -0400

    Initial commit

---
+++ CheckLuck/CheckLuck.cs
@@ -0,0 +1,44 @@
+﻿using TerrariaApi.Server;
+using Terraria;
+using TShockAPI;
+using Microsoft.Xna.Framework;
+
+namespace PluginTemplate;
+
+[ApiVersion(2, 1)]
+public class CheckLuck : TerrariaPlugin
+{
+    public override string Name => "Check luck";
+
+    public override Version Version => new Version(1, 0, 0);
+
+    public override string Author => "Maksym Ivanchenko";
+
+    public override string Description => "Add a command for luck checking.";
+
+    public CheckLuck(Main game) : base(game)
+    {
+    }
+
+    public override void Initialize()
+    {
+        Commands.ChatCommands.Add(new Command(SendLuck, "luck"));
+    }
+
+    private void SendLuck(CommandArgs args)
+    {
+        float luck = args.Player.TPlayer.luck;
+        string array = $"Current luck is {luck}.";
+        var color = luck switch
+        {
+            >= 0.75f => Color.DarkGreen,
+            >= 0.5f => Color.Green,
+            >= 0.25f => Color.LightGreen,
+            > 0f => Color.LimeGreen,
+            0f => Color.LightGray,
+            >= -0.15f => Color.Coral,
+            _ => Color.DarkRed,
+        };
+        args.Player.SendMessage(array, color);
+    }
+}
---
+++ README.md
@@ -0,0 +1,20 @@
+# Check Luck
+A simple plugin for TShock that allows players to check their current luck status using the `/luck` command.
+
+## Features
+* Adds a `/luck` command for players to check their current luck status in the game.
+* Displays the current luck value with a corresponding color based on the luck value range.
+
+## Installation
+1. Download the latest release from the Releases page.
+2. Copy the CheckLuck.dll file to the TShock server's "plugins" folder.
+3. Start or restart the TShock server.
+
+## Usage
+To check your current luck status in the game, type `/luck` in the chat.
+
+## Credits
+This plugin was created by Maksym Ivanchenko.
+
+## License
+This plugin is released under the [MIT License](https://github.com/vanchaxy/CheckLuck/blob/main/LICENSE).
